1b39e049d292421c4d3ce3f938af39a135c9984a
Juan Lopez <lopez.ju@husky.neu.edu>
2017-08-10 16:20:04 -0400

    updated settings: removed staticfiles_root

--- tweet_screen/settings.py
+++ tweet_screen/settings.py
@@ -20,9 +20,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 
 # Quick-start development settings - unsuitable for production
